d77722713204571844064a7030ca1cb806c14a24
Spencer W.S. James <spencer@kokoabim.com>
2012-02-17 21:25:33 -0800

    using header files from SpringBoard include directory instead of individual header file

--- NotificationCenter Widget.xctemplate/___PACKAGENAMEASIDENTIFIER___Controller.h
+++ NotificationCenter Widget.xctemplate/___PACKAGENAMEASIDENTIFIER___Controller.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <BBWeeAppController-Protocol.h>
+#import "SpringBoard/BBWeeAppController.h"
 
 @interface ___FILEBASENAMEASIDENTIFIER___ : NSObject <BBWeeAppController>
 {
