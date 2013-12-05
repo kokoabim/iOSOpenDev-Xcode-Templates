6251d4c462aa62537be8644976af6d76ea2d9606
Mokhlas Hussein <mokhleshussien@aol.com>
2013-12-05 21:16:23 +0100

    Added Velox Plugins Template

--- Velox Plugin.xctemplate/___PACKAGENAMEASIDENTIFIER___FolderView.m
+++ Velox Plugin.xctemplate/___PACKAGENAMEASIDENTIFIER___FolderView.m
@@ -4,6 +4,7 @@
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//  Velox Plugins Template Created By : @iMokhles
 //
 
 #import "VeloxFolderViewProtocol.h"
